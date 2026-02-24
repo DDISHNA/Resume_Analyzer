5afa6315b67fff847cddf1f2eaaa4be1c0781011
Dishna-cube <Dishna@cubehighways.com>
2026-02-24 23:37:34 +0530

    initial commit

---
+++ app/__init__.py
@@ -0,0 +1,21 @@
+# app/__init__.py
+
+from flask import Flask
+from config import Config
+from .extensions import db, login_manager, migrate
+
+def create_app():
+    app = Flask(__name__)
+    app.config.from_object(Config)
+
+    # Initialize extensions
+    db.init_app(app)
+    login_manager.init_app(app)
+    migrate.init_app(app, db)
+
+    # Simple test route
+    @app.route("/")
+    def home():
+        return "Resume Analyzer App Running 🚀"
+
+    return app
